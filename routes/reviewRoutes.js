d40c18c34faefbbf291ff411d598a0ec603aa4a8
Prasann199 <95702193+Prasann199@users.noreply.github.com>
2025-05-21 12:54:22 +0530

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const { addReview, getReviews, updateReview, deleteReview } = require('../Controllers/reviewController');
 
 router.post('/books/:id/reviews', authMiddleware, addReview); // POST /api/reviews/:bookId
-router.get('/getReview/:bookId', getReviews); // GET /api/reviews/:bookId
-router.put('/:id', authMiddleware, updateReview); // put /api/reviews/:bookId
-router.delete('/:id', authMiddleware, deleteReview); // POST /api/reviews/:bookId
+router.get('/getReview/:bookId', getReviews); // GET /api/getReviews/:bookId
+router.put('/:id', authMiddleware, updateReview); // put /api/reviews/:id
+router.delete('/:id', authMiddleware, deleteReview); // DELETE /api/reviews/:id
 module.exports = router;
